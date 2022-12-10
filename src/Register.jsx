a9727354bb9f304eba48ffd797b26def3104bb0b
228932 Malinowska Magdalena <228932@edu.p.lodz.pl>
2022-12-10 09:18:26 +0100

    wip

--- src/Register.jsx
+++ src/Register.jsx
@@ -1,4 +1,5 @@
 import React, {useState} from 'react';
+import validator from 'validator';
 export const Register = () => {
     const [login, setLogin] = useState('');
     const [name, setName] = useState('');
@@ -6,17 +7,47 @@ export const Register = () => {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
     const [confirmPassword, setConfirmPassword] = useState('');
+    const [passwordError, setPasswordError] = useState('');
+    const [passwordConfirmError, setPasswordConfirmError] = useState('');
+    const [registerButtonVisibility, setRegisterButtonVisibility] = useState(true);
 
     const handleSubmit = (e) => { 
         e.preventDefault();
+        validateConfirmPassword();
         console.log(login, name, surname, email, password);
+    }
+
+    const validateEmail = (e) => {
+        if (validator.isEmail(email)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    const validatePasswordOnChange = (e) => {
+        setPassword(e.target.value);
+        if (password.length >= 9) {
+            setPasswordError('');
+            setRegisterButtonVisibility(true);
+        } else {
+            setPasswordError('Password must be at least 9 characters.');
+            setRegisterButtonVisibility(false);
+        }
+    }
+
+    const validateConfirmPassword = () => {
+        if (password === confirmPassword) {
+            setPasswordConfirmError('');
+        } else {
+            setPasswordConfirmError('Passwords do not match.');
+        }
     }
 
 
     return (  
         <>
             <>Create an account</>
-            <></>
             <form onSubmit={handleSubmit}>
             <label for="name">Name</label>
                 <input value={name} onChange={(e) => setName(e.target.value)} type="name" placeholder="Name" id="name" name="name"></input>
@@ -27,11 +58,13 @@ export const Register = () => {
                 <label for="email">Email</label>
                 <input value={email} onChange={(e) => setEmail(e.target.value)} type="email" placeholder="Email" id="email" name="email"></input>
                 <label for="password">Password</label>
-                <input value={password} onChange={(e) => setPassword(e.target.value)} type="password" placeholder="Password" id="password" name="password"></input>
+                <input value={password} onChange={(e) => validatePasswordOnChange(e)} type="password" placeholder="Password" id="password" name="password"></input>
                 <>Passoword must be at least 9 characters.</>
+                <>{passwordError}</>
                 <label for="confirmPassword">Confirm password</label>
                 <input value={confirmPassword} onChange={(e) => setConfirmPassword(e.target.value)} type="password" placeholder="Confirm your password" id="confirmPassword" name="confirmPassword"></input>
-                <button type="submit">Register</button>
+                <>{passwordConfirmError}</>
+                <button type="submit" >Register</button>
             </form>
             <button>Already have an account? Log in here.</button>
         </>
